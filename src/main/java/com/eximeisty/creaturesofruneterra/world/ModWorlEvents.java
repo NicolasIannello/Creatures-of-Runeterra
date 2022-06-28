9817905c661348b7dd49ccf5e5a9d3b131ad0198
NicolasIannello <nicolas.iannello99@gmail.com>
2022-06-28 08:40:03 -0300

    tabs

--- src/main/java/com/eximeisty/creaturesofruneterra/world/ModWorlEvents.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/world/ModWorlEvents.java
@@ -5,9 +5,9 @@ import com.eximeisty.creaturesofruneterra.world.gen.ModEntityGeneration;
 import com.mojang.serialization.Codec;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
+//import net.minecraft.world.World;
 import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.FlatChunkGenerator;
+//import net.minecraft.world.gen.FlatChunkGenerator;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -32,23 +32,20 @@ public class ModWorlEvents {
             ServerWorld serverWorld = (ServerWorld) event.getWorld();
 
             try {
-                Method GETCODEC_METHOD =
-                        ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "func_230347_a_");
-                ResourceLocation cgRL = Registry.CHUNK_GENERATOR_CODEC.getKey(
-                        (Codec<? extends ChunkGenerator>)GETCODEC_METHOD.invoke(serverWorld.getChunkProvider().generator));
+                Method GETCODEC_METHOD = ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "func_230347_a_");
+                ResourceLocation cgRL = Registry.CHUNK_GENERATOR_CODEC.getKey((Codec<? extends ChunkGenerator>)GETCODEC_METHOD.invoke(serverWorld.getChunkProvider().generator));
 
                 if (cgRL != null && cgRL.getNamespace().equals("terraforged")) {
                     return;
                 }
             } catch (Exception e) {
-                LogManager.getLogger().error("Was unable to check if " + serverWorld.getDimensionKey().getLocation()
-                        + " is using Terraforged's ChunkGenerator.");
+                LogManager.getLogger().error("Was unable to check if " + serverWorld.getDimensionKey().getLocation()+" is using Terraforged's ChunkGenerator.");
             }
 
             // Prevent spawning our structure in Vanilla's superflat world
-            if (serverWorld.getChunkProvider().generator instanceof FlatChunkGenerator && serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
+            /*if (chunk instanceof FlatChunkGenerator && serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
                 return;
-            }
+            }*/
         }
     }
 }
