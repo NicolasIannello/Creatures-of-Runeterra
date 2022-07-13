37a6163a8c9a7a10554afd0ffd882869bb02cb0b
NicolasIannello <nicolas.iannello99@gmail.com>
2022-07-13 02:50:28 -0300

    attack animation

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/custom/XerSaiDunebreakerEntity.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/custom/XerSaiDunebreakerEntity.java
@@ -1,15 +1,25 @@
 package com.eximeisty.creaturesofruneterra.entity.custom;
 
+import java.util.EnumSet;
+import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.pathfinding.ClimberPathNavigator;
+import net.minecraft.pathfinding.Path;
+import net.minecraft.pathfinding.PathNavigator;
+import net.minecraft.util.EntityPredicates;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -18,21 +28,28 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 
-@SuppressWarnings("unchecked")
-public class XerSaiDunebreakerEntity extends XerSaiHatchlingEntity implements IAnimatable {
+//@SuppressWarnings("unchecked")
+public class XerSaiDunebreakerEntity extends CreatureEntity implements IAnimatable {
+    private static final DataParameter<Integer> STATE = EntityDataManager.createKey(XerSaiDunebreakerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Byte> CLIMBING = EntityDataManager.createKey(XerSaiDunebreakerEntity.class, DataSerializers.BYTE);
     private AnimationFactory factory = new AnimationFactory(this);
+    private int swingCooldown=0;
+    private int normalAttack=0;
 
-    public XerSaiDunebreakerEntity(EntityType<? extends XerSaiHatchlingEntity> type, World worldIn) {
+    public XerSaiDunebreakerEntity(EntityType<? extends CreatureEntity> type, World worldIn) {
         super(type, worldIn);
     }
     
     public static AttributeModifierMap.MutableAttribute setCustomAttributes(){
-        return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 6)
-        .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.15)
-        .createMutableAttribute(Attributes.ATTACK_DAMAGE, 2)
-        .createMutableAttribute(Attributes.FOLLOW_RANGE, 20)
+        return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 6)//60?
+        .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25)
+        .createMutableAttribute(Attributes.ATTACK_DAMAGE, 2)//15?
+        .createMutableAttribute(Attributes.FOLLOW_RANGE, 20)//30?
         .createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 0)
+        .createMutableAttribute(Attributes.KNOCKBACK_RESISTANCE, 10)
         .createMutableAttribute(Attributes.ATTACK_SPEED, 0.3);
     }
 
@@ -40,30 +57,235 @@ public class XerSaiDunebreakerEntity extends XerSaiHatchlingEntity implements IA
     protected void registerGoals(){
         super.registerGoals();
         this.goalSelector.addGoal( 1, new NearestAttackableTargetGoal<>( this, PlayerEntity.class, true ));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+        this.goalSelector.addGoal(2, new XerSaiDunebreakerEntity.MeleeAttackGoal(this, 1.0D, false));
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setCallsForHelp(XerSaiHatchlingEntity.class));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
         this.targetSelector.addGoal(7, new NearestAttackableTargetGoal<>(this, AbstractVillagerEntity.class, false));
         this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, IronGolemEntity.class, true));
     }
-    
-    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+
+    public <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.xersai_dunebreaker.walk", true));
             return PlayState.CONTINUE;
         }
+        // if ((this.dead || this.getHealth() < 0.01 || this.isDeadOrDying())) {
+		// 	event.getController().setAnimation(new AnimationBuilder().addAnimation("death", false));
+		// 	return PlayState.CONTINUE;
+		// }
 
         event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.xersai_dunebreaker.idle", true));
         return PlayState.CONTINUE;
     }
 
+    public <E extends IAnimatable> PlayState predicate2(AnimationEvent<E> event) {
+        if ( dataManager.get(STATE) == 1 ) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.xersai_dunebreaker.attack", true));
+			return PlayState.CONTINUE;
+		}
+        return PlayState.STOP;
+    }
+
     @Override
     public void registerControllers(AnimationData data){
         data.addAnimationController(new AnimationController(this, "controller", 0, this::predicate));
+        data.addAnimationController(new AnimationController(this, "attacks", 0, this::predicate2));
     }
 
     @Override
     public AnimationFactory getFactory(){
         return this.factory;
+    }
+
+    @Override
+    protected void updateLeashedState() {
+        super.updateLeashedState();
+        this.clearLeashed(true, true);
+        return;
+    }
+
+    protected PathNavigator createNavigator(World worldIn) {
+        return new ClimberPathNavigator(this, worldIn);
+    }
+    
+    protected void registerData() {
+        super.registerData();
+        this.dataManager.register(CLIMBING, (byte)0);
+        dataManager.register(STATE, 0);
+    }
+    
+    public void tick() {
+        super.tick();
+        if (!this.world.isRemote) {
+           this.setBesideClimbableBlock(this.collidedHorizontally);
+        }
+    }
+    
+    public boolean isOnLadder() {
+        return this.isBesideClimbableBlock();
+    }
+    
+    public boolean isBesideClimbableBlock() {
+        return (this.dataManager.get(CLIMBING) & 1) != 0;
+    }
+    
+    public void setBesideClimbableBlock(boolean climbing) {
+        byte b0 = this.dataManager.get(CLIMBING);
+        if (climbing) {
+           b0 = (byte)(b0 | 1);
+        } else {
+           b0 = (byte)(b0 & -2);
+        }
+        this.dataManager.set(CLIMBING, b0);
+    }
+
+    public class MeleeAttackGoal extends Goal {
+        protected final CreatureEntity attacker;
+        private final double speedTowardsTarget;
+        private final boolean longMemory;
+        private Path path;
+        private double targetX;
+        private double targetY;
+        private double targetZ;
+        private int delayCounter;        
+        private long lastCheckTime;
+        private int failedPathFindingPenalty = 0;
+        private boolean canPenalize = false;
+
+        public MeleeAttackGoal(CreatureEntity creature, double speedIn, boolean useLongMemory) {
+            this.attacker = creature;
+            this.speedTowardsTarget = speedIn;
+            this.longMemory = useLongMemory;
+            this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+        }
+        
+        public boolean shouldExecute() {
+            long i = this.attacker.world.getGameTime();
+            if (i - this.lastCheckTime < 20L) {
+                return false;
+            } else {
+                this.lastCheckTime = i;
+                LivingEntity livingentity = this.attacker.getAttackTarget();
+                if (livingentity == null) {
+                    return false;
+                } else if (!livingentity.isAlive()) {
+                    return false;
+                } else {
+                    if (canPenalize) {
+                        if (--this.delayCounter <= 0) {
+                            this.path = this.attacker.getNavigator().pathfind(livingentity, 0);
+                            this.delayCounter = 4 + this.attacker.getRNG().nextInt(7);
+                            return this.path != null;
+                        } else {
+                            return true;
+                        }
+                    }
+                    this.path = this.attacker.getNavigator().pathfind(livingentity, 0);
+                    if (this.path != null) {
+                        return true;
+                    } else {
+                        return this.getAttackReachSqr(livingentity) >= this.attacker.getDistanceSq(livingentity.getPosX(), livingentity.getPosY(), livingentity.getPosZ());
+                    }
+                }
+            }
+        }
+        
+        public boolean shouldContinueExecuting() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            if (livingentity == null) {
+                return false;
+            } else if (!livingentity.isAlive()) {
+                return false;
+            } else if (!this.longMemory) {
+                return !this.attacker.getNavigator().noPath();
+            } else if (!this.attacker.isWithinHomeDistanceFromPosition(livingentity.getPosition())) {
+                return false;
+            } else {
+                return !(livingentity instanceof PlayerEntity) || !livingentity.isSpectator() && !((PlayerEntity)livingentity).isCreative();
+            }
+        }
+        
+        public void startExecuting() {
+            //LogManager.getLogger().log(Level.DEBUG, "startExecuting");
+            this.attacker.getNavigator().setPath(this.path, this.speedTowardsTarget);
+            this.attacker.setAggroed(true);
+            this.delayCounter = 0;
+            swingCooldown = swingCooldown-1;
+        }
+        
+        public void resetTask() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            if (!EntityPredicates.CAN_AI_TARGET.test(livingentity)) {
+                this.attacker.setAttackTarget((LivingEntity)null);
+            }
+            this.attacker.setAggroed(false);
+            this.attacker.getNavigator().clearPath();
+        }
+        
+        public void tick() {
+            LivingEntity livingentity = this.attacker.getAttackTarget();
+            this.attacker.getLookController().setLookPositionWithEntity(livingentity, 30.0F, 30.0F);
+            double d0 = this.attacker.getDistanceSq(livingentity.getPosX(), livingentity.getPosY(), livingentity.getPosZ());
+            this.delayCounter = Math.max(this.delayCounter - 1, 0);
+            if ((this.longMemory || this.attacker.getEntitySenses().canSee(livingentity)) && this.delayCounter <= 0 && (this.targetX == 0.0D && this.targetY == 0.0D && this.targetZ == 0.0D || livingentity.getDistanceSq(this.targetX, this.targetY, this.targetZ) >= 1.0D || this.attacker.getRNG().nextFloat() < 0.05F)) {
+                this.targetX = livingentity.getPosX();
+                this.targetY = livingentity.getPosY();
+                this.targetZ = livingentity.getPosZ();
+                this.delayCounter = 4 + this.attacker.getRNG().nextInt(7);
+                if (this.canPenalize) {
+                    this.delayCounter += failedPathFindingPenalty;
+                    if (this.attacker.getNavigator().getPath() != null) {
+                        net.minecraft.pathfinding.PathPoint finalPathPoint = this.attacker.getNavigator().getPath().getFinalPathPoint();
+                        if (finalPathPoint != null && livingentity.getDistanceSq(finalPathPoint.x, finalPathPoint.y, finalPathPoint.z) < 1){
+                            failedPathFindingPenalty = 0;
+                        }else {
+                            failedPathFindingPenalty += 10;
+                        }
+                    }else {
+                        failedPathFindingPenalty += 10;
+                    }
+                }
+                if (d0 > 1024.0D) {
+                    this.delayCounter += 10;
+                } else if (d0 > 256.0D) {
+                    this.delayCounter += 5;
+                }
+                if (!this.attacker.getNavigator().tryMoveToEntityLiving(livingentity, this.speedTowardsTarget)) {
+                    this.delayCounter += 15;
+                }
+            }
+            //LogManager.getLogger().log(Level.DEBUG, "CD "+swingCooldown);
+            swingCooldown = swingCooldown - 1;
+            this.checkAndPerformAttack(livingentity, d0);
+        }
+        
+        protected void checkAndPerformAttack(LivingEntity enemy, double distToEnemySqr) {
+            double d0 = this.attacker.getDistanceSq(enemy.getPosX(), enemy.getPosY(), enemy.getPosZ());
+            if (d0 <= 17 && swingCooldown <= 0) {
+                ++normalAttack;
+                LogManager.getLogger().log(Level.DEBUG, "Attack");
+                this.resetSwingCooldown();             
+                dataManager.set(STATE, 1);
+            }
+            if(normalAttack>0){
+                ++normalAttack;
+                if(normalAttack==10 && d0 <= 17){
+                    //LogManager.getLogger().log(Level.DEBUG, "Hit");
+                    this.attacker.attackEntityAsMob(enemy);
+                }
+                if(normalAttack>=20){
+                    dataManager.set(STATE, 0);
+                    normalAttack=0;
+                }
+            }
+        }
+        
+        protected void resetSwingCooldown() {
+            swingCooldown = 90;
+        }
+        
+        protected double getAttackReachSqr(LivingEntity attackTarget) {
+            return (double)(this.attacker.getWidth() * 2.0F * this.attacker.getWidth() * 2.0F + attackTarget.getWidth());
+        }
     }
 }
