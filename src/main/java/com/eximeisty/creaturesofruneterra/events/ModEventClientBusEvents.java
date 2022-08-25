acfd692e30aef9c3682fdd7dcfa5b68c1d64179c
NicolasIannello <nicolas.iannello99@gmail.com>
2022-08-25 19:08:11 -0300

    drill render registry

--- src/main/java/com/eximeisty/creaturesofruneterra/events/ModEventClientBusEvents.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/events/ModEventClientBusEvents.java
@@ -1,11 +1,14 @@
 package com.eximeisty.creaturesofruneterra.events;
 
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.block.ModTiles;
+import com.eximeisty.creaturesofruneterra.block.entity.client.drill.DrillRenderer;
 import com.eximeisty.creaturesofruneterra.item.custom.SaiArmorItem;
 import com.eximeisty.creaturesofruneterra.entity.client.armor.sai.SaiArmorRenderer;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
@@ -15,5 +18,10 @@ public class ModEventClientBusEvents {
     @SubscribeEvent
     public static void registerArmorRenderers(final FMLClientSetupEvent event){
         GeoArmorRenderer.registerArmorRenderer(SaiArmorItem.class, () -> new SaiArmorRenderer());
+    }
+
+    @SubscribeEvent
+    public static void registerBlockRenderers(final FMLClientSetupEvent event){
+        ClientRegistry.bindTileEntityRenderer(ModTiles.DRILL.get(), DrillRenderer::new);
     }
 }
