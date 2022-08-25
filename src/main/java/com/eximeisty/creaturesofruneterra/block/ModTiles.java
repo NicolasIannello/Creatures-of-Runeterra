eaa98a444578d1cfbdc399a8d82bada032204f57
NicolasIannello <nicolas.iannello99@gmail.com>
2022-08-25 19:06:35 -0300

    tiles registry

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/ModTiles.java
@@ -0,0 +1,21 @@
+package com.eximeisty.creaturesofruneterra.block;
+
+import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.block.entity.DrillTileEntity;
+
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModTiles {
+    public static final DeferredRegister<TileEntityType<?>> TILES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, CreaturesofRuneterra.MOD_ID);
+
+    public static final RegistryObject<TileEntityType<DrillTileEntity>> DRILL = TILES.register("drill", 
+    ()-> TileEntityType.Builder.create(DrillTileEntity::new, ModBlocks.DRILL.get()).build(null));
+
+    public static void register(IEventBus eventBus){
+        TILES.register(eventBus);
+    }
+}
