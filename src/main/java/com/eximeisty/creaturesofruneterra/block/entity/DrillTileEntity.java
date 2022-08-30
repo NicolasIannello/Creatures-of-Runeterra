480c63c99e0b1714b77339154e861e4f79410704
NicolasIannello <nicolas.iannello99@gmail.com>
2022-08-30 01:50:05 -0300

    trans done / crashed on exit

--- src/main/java/com/eximeisty/creaturesofruneterra/block/entity/DrillTileEntity.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/entity/DrillTileEntity.java
@@ -1,24 +1,38 @@
 package com.eximeisty.creaturesofruneterra.block.entity;
 
+import javax.annotation.Nullable;
+
 import com.eximeisty.creaturesofruneterra.block.ModTiles;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class DrillTileEntity extends TileEntity implements IAnimatable{
+public class DrillTileEntity extends TileEntity implements IAnimatable, ITickableTileEntity{
     private final AnimationFactory factory = new AnimationFactory(this);
+    public int estado=0;
 
     public DrillTileEntity() {
         super(ModTiles.DRILL.get());
     }
 
 	private <E extends TileEntity & IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-		return PlayState.CONTINUE;
+        if(this.getTileData().getInt("state")==1){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.drill.trans", false).addAnimation("animation.drill.drill", true));
+        }else if(this.getTileData().getInt("state")==2){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.drill.trans2", false));
+        }
+        return PlayState.CONTINUE;
 	}
 
     @Override
@@ -30,4 +44,47 @@ public class DrillTileEntity extends TileEntity implements IAnimatable{
     public AnimationFactory getFactory() {
         return this.factory;
     }
+
+    @Override
+	public CompoundNBT write(CompoundNBT compound) {
+		compound.putInt("state", this.estado);
+        return super.write(compound);
+	}
+
+	@Override
+	public void read(BlockState state, CompoundNBT compound) {
+		super.read(state, compound);
+	}
+
+    @Override @Nullable
+    public SUpdateTileEntityPacket getUpdatePacket() {
+        return new SUpdateTileEntityPacket(this.pos, 3/*3 4 -1 9*/, this.getUpdateTag());
+    }
+ 
+    @Override
+    public CompoundNBT getUpdateTag() {
+        return this.write(this.getTileData());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+        super.onDataPacket(net, pkt);
+        handleUpdateTag(this.world.getBlockState(this.getPos()), pkt.getNbtCompound());
+    }
+
+    @Override
+	public void handleUpdateTag(BlockState state, CompoundNBT tag) {
+		this.read(state, tag);
+	}
+    
+    @Override
+    public void tick() {
+
+    }
+
+    public void setEstado() {
+        if(this.estado==0 || this.estado==2){ this.estado=1; }else{ this.estado=2; }
+        //this.markDirty();
+        this.world.notifyBlockUpdate(this.pos, this.getBlockState(), this.getBlockState(), 2);
+	}
 }
