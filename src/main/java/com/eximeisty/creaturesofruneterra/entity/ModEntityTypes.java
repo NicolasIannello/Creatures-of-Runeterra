ec885c481f0d603166fce8ea4df3c14d0236f538
NicolasIannello <nicolas.iannello99@gmail.com>
2022-09-11 09:04:32 -0300

    multi parts / reksai size

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/ModEntityTypes.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/ModEntityTypes.java
@@ -4,6 +4,7 @@ import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.HexcoreEntity;
 import com.eximeisty.creaturesofruneterra.entity.custom.MisilEntity;
 import com.eximeisty.creaturesofruneterra.entity.custom.RekSaiEntity;
+import com.eximeisty.creaturesofruneterra.entity.custom.CoRPartEntity;
 import com.eximeisty.creaturesofruneterra.entity.custom.XerSaiDunebreakerEntity;
 import com.eximeisty.creaturesofruneterra.entity.custom.XerSaiHatchlingEntity;
 import com.eximeisty.creaturesofruneterra.entity.custom.XerxarethEntity;
@@ -38,12 +39,31 @@ public class ModEntityTypes {
 
     public static final RegistryObject<EntityType<RekSaiEntity>> REKSAI=
     ENTITY_TYPES.register("reksai", 
-        ()-> EntityType.Builder.create(RekSaiEntity::new, EntityClassification.MONSTER).immuneToFire().size(5f,5f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "reksai").toString())
+        ()-> EntityType.Builder.create(RekSaiEntity::new, EntityClassification.MONSTER).immuneToFire().size(2f,3f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "reksai").toString())
     );
 
+    //PART ENTITIES
+    public static final RegistryObject<EntityType<CoRPartEntity>> WIVHIV=
+    ENTITY_TYPES.register("wichiv", 
+        ()-> EntityType.Builder.create(CoRPartEntity::new, EntityClassification.MONSTER).immuneToFire().size(4f,4f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "wichiv").toString())
+    );
+    public static final RegistryObject<EntityType<CoRPartEntity>> WVIHV=
+    ENTITY_TYPES.register("wvihv", 
+        ()-> EntityType.Builder.create(CoRPartEntity::new, EntityClassification.MONSTER).immuneToFire().size(6f,5f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "wvihv").toString())
+    );
+    public static final RegistryObject<EntityType<CoRPartEntity>> WVIIHVI=
+    ENTITY_TYPES.register("wviihvi", 
+        ()-> EntityType.Builder.create(CoRPartEntity::new, EntityClassification.MONSTER).immuneToFire().size(7f,6f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "wviihvi").toString())
+    );
+    public static final RegistryObject<EntityType<CoRPartEntity>> WIIIHIII=
+    ENTITY_TYPES.register("wiiihiii", 
+        ()-> EntityType.Builder.create(CoRPartEntity::new, EntityClassification.MONSTER).immuneToFire().size(3f,3f).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "wiiihiii").toString())
+    );
+
+    //ITEMS
     public static final RegistryObject<EntityType<MisilEntity>> MISIL = 
     ENTITY_TYPES.register("misil", 
-        ()-> EntityType.Builder.<MisilEntity>create(MisilEntity::new, EntityClassification.MISC).size(4.0F, 2.0F).trackingRange(4).updateInterval(20).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "misil").toString())
+        ()-> EntityType.Builder.<MisilEntity>create(MisilEntity::new, EntityClassification.MISC).size(2.0F, 2.0F).trackingRange(4).updateInterval(20).build(new ResourceLocation(CreaturesofRuneterra.MOD_ID, "misil").toString())
     );
 
     public static final RegistryObject<EntityType<HexcoreEntity>> HEXCORE = 
