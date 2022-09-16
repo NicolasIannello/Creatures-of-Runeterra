1d5ee3552b71d4c47cf4104a7c724fad6e2fd7ea
NicolasIannello <nicolas.iannello99@gmail.com>
2022-09-16 12:57:23 -0300

    undewater walkthrough block

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/custom/EmptyBlock.java
@@ -0,0 +1,27 @@
+package com.eximeisty.creaturesofruneterra.block.custom;
+
+import net.minecraft.block.AbstractCoralPlantBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+
+public class EmptyBlock extends AbstractCoralPlantBlock{
+    private static final VoxelShape SHAPE = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+
+    public EmptyBlock(Properties properties) {
+        super(properties);
+    }
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        return true;
+    }
+}
