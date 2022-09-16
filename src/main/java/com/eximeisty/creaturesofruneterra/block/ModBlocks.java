f387bd7e9fde8ac732bb57906df740bc5659c639
NicolasIannello <nicolas.iannello99@gmail.com>
2022-09-16 12:58:40 -0300

    tendril + flag for register block item

--- src/main/java/com/eximeisty/creaturesofruneterra/block/ModBlocks.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/ModBlocks.java
@@ -4,10 +4,15 @@ import java.util.function.Supplier;
 
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.block.custom.DrillBlock;
+import com.eximeisty.creaturesofruneterra.block.custom.EmptyBlock;
+import com.eximeisty.creaturesofruneterra.item.ModItemGroup;
+import com.eximeisty.creaturesofruneterra.item.ModItems;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -18,17 +23,20 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS= DeferredRegister.create(ForgeRegistries.BLOCKS, CreaturesofRuneterra.MOD_ID);
 
     public static final RegistryObject<Block> DRILL= registerBlock("drill", () -> new DrillBlock(AbstractBlock.Properties.create(Material.ROCK)
-    .harvestLevel(3).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5F).notSolid()));
+    .harvestLevel(3).harvestTool(ToolType.PICKAXE).setRequiresTool().hardnessAndResistance(5F).notSolid()), false);
 
-    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
+    public static final RegistryObject<Block> TENDRIL= registerBlock("tendril", () -> new EmptyBlock(AbstractBlock.Properties.create(Material.LEAVES)
+    .hardnessAndResistance(0.2F).notSolid().doesNotBlockMovement()), true);
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, boolean flag){
         RegistryObject<T> toReturn= BLOCKS.register(name, block);
-        //registerBlockItem(name, toReturn);
+        if(flag) registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    /*private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
         ModItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties().group(ModItemGroup.COR_GROUP)));
-    }*/
+    }
 
     public static void register(IEventBus eventBus){
         BLOCKS.register(eventBus);
