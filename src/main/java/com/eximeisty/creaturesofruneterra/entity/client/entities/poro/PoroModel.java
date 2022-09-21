9695a63d8d8efc49e7881934a316d66b9c64620e
NicolasIannello <nicolas.iannello99@gmail.com>
2022-09-21 19:30:39 -0300

    poro model

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/poro/PoroModel.java
@@ -0,0 +1,25 @@
+package com.eximeisty.creaturesofruneterra.entity.client.entities.poro;
+
+import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.entity.custom.PoroEntity;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class PoroModel extends AnimatedGeoModel<PoroEntity> {
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(PoroEntity animatable) {
+        return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "animations/entities/poro.animation.json");
+    }
+
+    @Override
+    public ResourceLocation getModelLocation(PoroEntity object) {
+        return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "geo/entities/poro.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(PoroEntity object) {
+        return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/poro.png");
+    }
+    
+}
