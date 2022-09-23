198fbe5351e134259c7a4f43b3d3f9ea882aad36
NicolasIannello <nicolas.iannello99@gmail.com>
2022-09-23 20:46:00 -0300

    -renderrecursively

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/poro/PoroRenderer.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/poro/PoroRenderer.java
@@ -6,13 +6,8 @@ import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-import net.minecraft.client.renderer.RenderType;
-
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-
 public class PoroRenderer extends GeoEntityRenderer<PoroEntity> {
+    private ResourceLocation rl=new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/poro.png");
 
     public PoroRenderer(EntityRendererManager renderManager) {
         super(renderManager, new PoroModel());
@@ -20,12 +15,22 @@ public class PoroRenderer extends GeoEntityRenderer<PoroEntity> {
 
     @Override
     public ResourceLocation getEntityTexture(PoroEntity entity) {
-        return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/poro.png");
+        return rl;
     }
 
-    @Override
-	public RenderType getRenderType(PoroEntity animatable, float partialTicks, MatrixStack stack, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
-        stack.scale(1F, 1F, 1F);
-        return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
-	}
+    // @Override
+	// public void renderRecursively(GeoBone bone, MatrixStack stack, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+	// 	if (bone.getName().equals("itemBone")){
+	// 		stack.push();
+	// 		stack.rotate(Vector3f.XP.rotationDegrees(-75));
+	// 		stack.rotate(Vector3f.YP.rotationDegrees(0));
+	// 		stack.rotate(Vector3f.ZP.rotationDegrees(0));
+	// 		stack.translate(0.2D, 0.4D, 0D);
+	// 		stack.scale(1.0f, 1.0f, 1.0f);
+	// 		Minecraft.getInstance().getItemRenderer().renderItem(poroWeapon, TransformType.THIRD_PERSON_RIGHT_HAND, packedLightIn, packedOverlayIn, stack, this.rtb);
+	// 		stack.pop();
+	// 		bufferIn = rtb.getBuffer(RenderType.getEntityTranslucent(whTexture));
+	// 	}
+	// 	super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+	// }
 }
