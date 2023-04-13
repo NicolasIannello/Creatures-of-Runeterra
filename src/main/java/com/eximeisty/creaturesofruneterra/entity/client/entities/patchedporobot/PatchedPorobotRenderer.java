68a4b1fd3927a4349d706610235e3707d1ac4063
NicolasIannello <nicolas.iannello99@gmail.com>
2023-04-12 23:55:24 -0300

    porobot renderer

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/patchedporobot/PatchedPorobotRenderer.java
@@ -0,0 +1,21 @@
+package com.eximeisty.creaturesofruneterra.entity.client.entities.patchedporobot;
+
+import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.entity.custom.PatchedPorobotEntity;
+
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class PatchedPorobotRenderer extends GeoEntityRenderer<PatchedPorobotEntity> {
+    private ResourceLocation rl=new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/poro.png");
+
+    public PatchedPorobotRenderer(EntityRendererManager renderManager) {
+        super(renderManager, new PatchedPorobotModel());
+    }
+
+    @Override
+    public ResourceLocation getEntityTexture(PatchedPorobotEntity entity) {
+        return rl;
+    }
+}
