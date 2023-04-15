914ab6d3fd6150a9c1ccd52afbf1fd76091a11b2
NicolasIannello <nicolas.iannello99@gmail.com>
2023-04-15 14:17:41 -0300

    porobot container

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/container/PorobotContainer.java
@@ -0,0 +1,104 @@
+package com.eximeisty.creaturesofruneterra.container;
+
+import com.eximeisty.creaturesofruneterra.entity.custom.PatchedPorobotEntity;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+
+
+public class PorobotContainer extends Container {
+    private final PatchedPorobotEntity poro;
+    private final PlayerEntity playerEntity;
+    private final IItemHandler playerInventory;
+
+    public PorobotContainer(int windowId, World world, int id, PlayerInventory playerInventory, PlayerEntity player) {
+        super(ModContainers.POROBOT_CONTAINER.get(), windowId);
+
+        this.poro=(PatchedPorobotEntity)world.getEntityByID(id);
+        this.playerEntity= player;
+        this.playerInventory= new InvWrapper(playerInventory);
+        layoutPlayerInventorySlots(8, 86);
+
+        if(poro!=null){
+            poro.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h ->{
+                addSlot(new SlotItemHandler(h, 0, 80, 31));
+                addSlot(new SlotItemHandler(h, 1, 80, 53));
+            });
+        }
+    }
+
+    @Override
+    public boolean canInteractWith(PlayerEntity playerIn) {
+        return playerEntity.getDistance(poro)<5;
+    }
+
+    private int addSlotRange(IItemHandler handler, int index, int x, int y, int amount, int dx) {
+        for (int i = 0; i < amount; i++) {
+            addSlot(new SlotItemHandler(handler, index, x, y));
+            x += dx;
+            index++;
+        }
+        return index;
+    }
+
+    private int addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
+        for (int j = 0; j < verAmount; j++) {
+            index = addSlotRange(handler, index, x, y, horAmount, dx);
+            y += dy;
+        }
+        return index;
+    }
+
+    private void layoutPlayerInventorySlots(int leftCol, int topRow) {
+        addSlotBox(playerInventory, 9, leftCol, topRow, 9, 18, 3, 18);
+        topRow += 58;
+        addSlotRange(playerInventory, 0, leftCol, topRow, 9, 18);
+    }
+    
+    private static final int HOTBAR_SLOT_COUNT = 9;
+    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
+    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
+
+    // THIS YOU HAVE TO DEFINE!
+    private static final int TE_INVENTORY_SLOT_COUNT = 2;  // must match TileEntityInventoryBasic.NUMBER_OF_SLOTS
+
+    @Override
+    public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
+        Slot sourceSlot = inventorySlots.get(index);
+        if (sourceSlot == null || !sourceSlot.getHasStack()) return ItemStack.EMPTY;
+        ItemStack sourceStack = sourceSlot.getStack();
+        ItemStack copyOfSourceStack = sourceStack.copy();
+
+        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+            if (!mergeItemStack(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;
+            }
+        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
+            if (!mergeItemStack(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;
+            }
+        } else {
+            System.out.println("Invalid slotIndex:" + index);
+            return ItemStack.EMPTY;
+        }
+        if (sourceStack.getCount() == 0) {
+            sourceSlot.putStack(ItemStack.EMPTY);
+        } else {
+            sourceSlot.onSlotChanged();
+        }
+        sourceSlot.onTake(playerEntity, sourceStack);
+        return copyOfSourceStack;
+    }
+}
