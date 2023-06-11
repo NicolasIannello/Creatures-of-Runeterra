53fd308c3b57434046e4365153ef76722c0e82eb
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-11 19:24:12 -0300

    - getRenderType

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/bullet/BulletRenderer.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/bullet/BulletRenderer.java
@@ -2,11 +2,7 @@ package com.eximeisty.creaturesofruneterra.entity.client.entities.bullet;
 
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.BulletEntity;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.GeoProjectilesRenderer;
@@ -22,9 +18,4 @@ public class BulletRenderer extends GeoProjectilesRenderer<BulletEntity> {
         return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/bullet.png");
     }
 
-    @Override
-	public RenderType getRenderType(BulletEntity animatable, float partialTicks, MatrixStack stack, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
-        stack.scale(1F, 1F, 1F);
-        return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
-	}
 }
--- src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/misil/MisilRenderer.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/misil/MisilRenderer.java
@@ -2,11 +2,6 @@ package com.eximeisty.creaturesofruneterra.entity.client.entities.misil;
 
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.MisilEntity;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.GeoProjectilesRenderer;
@@ -22,9 +17,4 @@ public class MisilRenderer extends GeoProjectilesRenderer<MisilEntity> {
         return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/misil.png");
     }
 
-    @Override
-	public RenderType getRenderType(MisilEntity animatable, float partialTicks, MatrixStack stack, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
-        stack.scale(1F, 1F, 1F);
-        return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
-	}
 }
