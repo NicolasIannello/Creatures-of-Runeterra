17467a3c804de84db0344f186cd3cdae70aeb51e
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-11 19:24:42 -0300

    - getRenderType + head rotations

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/dunebreaker/XerSaiDunebreakerModel.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/client/entities/dunebreaker/XerSaiDunebreakerModel.java
@@ -1,9 +1,14 @@
 package com.eximeisty.creaturesofruneterra.entity.client.entities.dunebreaker;
 
+import javax.annotation.Nullable;
+
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.XerSaiDunebreakerEntity;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class XerSaiDunebreakerModel extends AnimatedGeoModel<XerSaiDunebreakerEntity> {
 
@@ -21,5 +26,16 @@ public class XerSaiDunebreakerModel extends AnimatedGeoModel<XerSaiDunebreakerEn
     public ResourceLocation getTextureLocation(XerSaiDunebreakerEntity object) {
         return new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/xersai_dunebreaker.png");
     }
-    
+
+    @Override @SuppressWarnings({"rawtypes","unchecked"})
+	public void setLivingAnimations(XerSaiDunebreakerEntity entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+		super.setLivingAnimations(entity, uniqueID, customPredicate);
+		IBone head = this.getAnimationProcessor().getBone("Head");
+
+		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		if (head != null && (Integer)entity.getDataManager().getAll().get(15).getValue()==0) {
+			head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+			head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
+		}
+	}
 }
