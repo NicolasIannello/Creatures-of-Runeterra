f069d46113b305785981a6d46817ec0877695f66
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-22 23:20:48 -0300

    -logger

--- src/main/java/com/eximeisty/creaturesofruneterra/CreaturesofRuneterra.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/CreaturesofRuneterra.java
@@ -2,7 +2,6 @@ package com.eximeisty.creaturesofruneterra;
 
 import com.eximeisty.creaturesofruneterra.item.ModCreativeModeTabs;
 import com.eximeisty.creaturesofruneterra.item.ModItems;
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -12,12 +11,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(CreaturesofRuneterra.MOD_ID)
 public class CreaturesofRuneterra{
     public static final String MOD_ID = "creaturesofruneterra";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public CreaturesofRuneterra(){
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -42,7 +39,6 @@ public class CreaturesofRuneterra{
 
 //    @SubscribeEvent
 //    public void onServerStarting(ServerStartingEvent event) {
-//        LOGGER.info("HELLO from server starting");
 //    }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
