2ed2692d81c00c255a234fe8366b00934604327e
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-22 22:17:31 -0300

    1.19.4 first

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/CreaturesofRuneterra.java
@@ -0,0 +1,46 @@
+package com.eximeisty.creaturesofruneterra;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CreaturesofRuneterra.MOD_ID)
+public class CreaturesofRuneterra{
+    public static final String MOD_ID = "creaturesofruneterra";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreaturesofRuneterra(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+    }
+
+//    @SubscribeEvent
+//    public void onServerStarting(ServerStartingEvent event) {
+//        LOGGER.info("HELLO from server starting");
+//    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
