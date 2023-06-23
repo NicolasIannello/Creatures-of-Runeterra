a809564958eb87526b2eb7dc3807338ba4fa901d
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-23 20:33:19 -0300

    Mod Blocks

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package com.eximeisty.creaturesofruneterra.block;
+
+import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.block.custom.EmptyBlock;
+import com.eximeisty.creaturesofruneterra.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, CreaturesofRuneterra.MOD_ID);
+
+    public static final RegistryObject<Block> TENDRIL = registerBlock("tendril",
+            () -> new EmptyBlock(BlockBehaviour.Properties.of(Material.LEAVES)
+                    .strength(0.2F).noOcclusion().noCollission()));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
