3125defcfb3d2875acf70408bd8ce51b138db22d
NicolasIannello <nicolas.iannello99@gmail.com>
2023-06-25 20:23:53 -0300

    dunebreaker spawn egg

--- src/main/java/com/eximeisty/creaturesofruneterra/item/ModItems.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/item/ModItems.java
@@ -1,7 +1,9 @@
 package com.eximeisty.creaturesofruneterra.item;
 
 import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.entity.ModEntities;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,6 +24,11 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> REKSAI_CLAW = ITEMS.register("reksai_claw",
             () -> new Item(new Item.Properties()));
+
+    //SPAWN EGGS
+    public static final RegistryObject<Item> XERSAI_DUNEBREAKER_SPAWN_EGG = ITEMS.register("xersai_dunebreaker_spawn_egg",
+            () -> new ForgeSpawnEggItem(ModEntities.XERSAI_DUNEBREAKER, 0x9F2BAF, 0x370E45,
+                    new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
