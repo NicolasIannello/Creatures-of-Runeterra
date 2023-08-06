af442325cab847efb2902e5a552819e16536e483
NicolasIannello <nicolas.iannello99@gmail.com>
2023-08-05 23:56:41 -0300

    another flag

--- src/main/java/com/eximeisty/creaturesofruneterra/mixin/MixinArmorStandEntity.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/mixin/MixinArmorStandEntity.java
@@ -15,18 +15,38 @@ public class MixinArmorStandEntity {
     private void onTick(CallbackInfo info){
         ArmorStandEntity clase = (ArmorStandEntity)((Object)this);
 
-        boolean flag1 = clase.getArmorInventoryList().toString().contains("fiddle_chestplate") && clase.getArmorInventoryList().toString().contains("fiddle_helmet");
+        boolean flag0 = clase.world.getDayTime()>=16000 && clase.world.getDayTime()<=19000;
+        boolean flag1 = clase.getArmorInventoryList().toString().equalsIgnoreCase("[1 chainmail_boots, 1 iron_leggings, 1 fiddle_chestplate, 1 fiddle_helmet]");
         boolean flag2 = true;
+        boolean flag3 = true;
 
-        int x = 2, r = 5;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < r ; j++) {
-                if(clase.world.getBlockState(clase.getPosition().add(-x+j, -0.5, -4+i))!=Blocks.SOUL_SOIL.getDefaultState()) flag2=false;
+        if(flag0){
+            if(flag1){
+                for (int l = 0; l < 4; l++) {
+                    int x = l%2==0 ? 2 : -2;
+                    int z = !(l%2==0) ? 4 : -4;
+                    if(clase.world.getBlockState(clase.getPosition().add(x, +0.5, z))!=Blocks.SOUL_SAND.getDefaultState()) flag2=false;
+                    if(clase.world.getBlockState(clase.getPosition().add(z, +0.5, x))!=Blocks.SOUL_SAND.getDefaultState()) flag2=false;
+                    if(clase.world.getBlockState(clase.getPosition().add(x, +1.5, z))!=Blocks.SOUL_FIRE.getDefaultState()) flag2=false;
+                    if(clase.world.getBlockState(clase.getPosition().add(z, +1.5, x))!=Blocks.SOUL_FIRE.getDefaultState()) flag2=false;
+                }
+                if(flag2){
+                    int x = 2, r = 5;
+                    for (int i = 0; i < 9; i++) {
+                        for (int j = 0; j < r ; j++) {
+                            if(clase.world.getBlockState(clase.getPosition().add(-x+j, -0.5, -4+i))!=Blocks.SOUL_SOIL.getDefaultState()) flag3=false;
+                        }
+                        if(i<=1){ x++; r+=2;}
+                        if(i>=6){ x--; r+=-2;}
+                    }
+                    if(flag3){
+                        //fiddle
+                    }
+                }
             }
-            if(i<=1){ x++; r+=2;}
-            if(i>=6){ x--; r+=-2;}
         }
 
-        System.out.println("f1:"+flag1+" f2:"+flag2);
+        //clase.remove();
+        System.out.println("f0:"+flag0+" f1:"+flag1+" f2:"+flag2+" f3:"+flag3);
     }
 }
