ab44216b32decefb2b4ec59f3adb4429dffb106e
NicolasIannello <nicolas.iannello99@gmail.com>
2023-08-10 23:42:35 -0300

    wither effect

--- src/main/java/com/eximeisty/creaturesofruneterra/item/custom/FiddleScythe.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/item/custom/FiddleScythe.java
@@ -1,6 +1,7 @@
 package com.eximeisty.creaturesofruneterra.item.custom;
 
 import com.eximeisty.creaturesofruneterra.item.client.fiddleScythe.FiddleScytheRenderer;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.IItemTier;
@@ -20,11 +21,15 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 public class FiddleScythe extends SwordItem implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
     public String controllerName = "controller";
-    //protected static final UUID REACH = UUID.fromString("f7e4f5c8-2463-4660-bfd3-0de7ac8f7720");
-    //private AttributeModifier reachModifier = new AttributeModifier(REACH, "Weapon modifier", 2, Operation.ADDITION);
+    // protected static final UUID REACH = UUID.fromString("f7e4f5c8-2463-4660-bfd3-0de7ac8f7720");
+    // private AttributeModifier reachModifier, damageModifier, speedModifier;
+    // private Multimap<Attribute, AttributeModifier> attributeModifiers;
 
     public FiddleScythe(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builderIn) {
         super(tier, attackDamageIn, attackSpeedIn, builderIn.setISTER(()-> FiddleScytheRenderer::new));
+        // reachModifier = new AttributeModifier(REACH, "reach_distance", 2, AttributeModifier.Operation.ADDITION);
+        // damageModifier = new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (float)attackDamageIn+tier.getAttackDamage(), AttributeModifier.Operation.ADDITION);
+        // speedModifier = new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", attackSpeedIn, AttributeModifier.Operation.ADDITION);
     }
 
     public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
@@ -54,7 +59,7 @@ public class FiddleScythe extends SwordItem implements IAnimatable {
     @Override
     public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         attacker.heal(1);
-        target.addPotionEffect(new EffectInstance(Effects.WITHER, 20*3));
+        target.addPotionEffect(new EffectInstance(Effects.WITHER, 20*4));
         return super.hitEntity(stack, target, attacker);
     }
 
@@ -72,4 +77,12 @@ public class FiddleScythe extends SwordItem implements IAnimatable {
         return factory;
     }
     
+    // public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+    //     Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+    //     builder.put(ForgeMod.REACH_DISTANCE.get(), reachModifier);
+    //     builder.put(Attributes.ATTACK_DAMAGE, damageModifier);
+    //     builder.put(Attributes.ATTACK_SPEED, speedModifier);
+    //     this.attributeModifiers = builder.build();
+    //     return equipmentSlot == EquipmentSlotType.MAINHAND ? attributeModifiers : super.getAttributeModifiers(equipmentSlot);
+    // }
 }
