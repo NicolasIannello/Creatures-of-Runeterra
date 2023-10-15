84912233a68d4e1213c0d6ed4396d85330766644
NicolasIannello <nicolas.iannello99@gmail.com>
2023-10-14 22:13:06 -0300

    armors

---
+++ src/main/java/com/eximeisty/creaturesofruneterra/events/ModEventClientBusEvents.java
@@ -0,0 +1,30 @@
+package com.eximeisty.creaturesofruneterra.events;
+
+import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
+import com.eximeisty.creaturesofruneterra.item.custom.FiddleArmorItem;
+import com.eximeisty.creaturesofruneterra.item.custom.SaiArmorItem;
+import com.eximeisty.creaturesofruneterra.item.custom.SaiElytraItem;
+import com.eximeisty.creaturesofruneterra.entity.client.armor.fiddle.FiddleArmorRenderer;
+import com.eximeisty.creaturesofruneterra.entity.client.armor.sai.SaiArmorRenderer;
+import com.eximeisty.creaturesofruneterra.entity.client.armor.sai.SaiElytraRenderer;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
+
+@Mod.EventBusSubscriber(modid = CreaturesofRuneterra.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModEventClientBusEvents {
+    @SubscribeEvent
+    public static void registerArmorRenderers(final FMLClientSetupEvent event){
+        GeoArmorRenderer.registerArmorRenderer(SaiArmorItem.class, () -> new SaiArmorRenderer());
+        GeoArmorRenderer.registerArmorRenderer(SaiElytraItem.class, () -> new SaiElytraRenderer());
+        GeoArmorRenderer.registerArmorRenderer(FiddleArmorItem.class, () -> new FiddleArmorRenderer());
+    }
+
+//    @SubscribeEvent
+//    public static void registerBlockRenderers(final FMLClientSetupEvent event){
+//        ClientRegistry.bindTileEntityRenderer(ModTiles.DRILL.get(), DrillRenderer::new);
+//    }
+}
