5b78e985dfc4807f4f5dfe850b54e27521b4b2e2
NicolasIannello <nicolas.iannello99@gmail.com>
2023-10-16 18:11:51 -0300

    import

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/render/XerSaiHatchlingRenderer.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/render/XerSaiHatchlingRenderer.java
@@ -4,7 +4,6 @@ import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.XerSaiHatchlingEntity;
 import com.eximeisty.creaturesofruneterra.entity.model.XerSaiHatchlingModel;
 
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
