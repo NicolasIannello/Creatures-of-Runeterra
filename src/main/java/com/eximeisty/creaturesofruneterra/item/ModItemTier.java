09c741fa64647ba2df20dc860e332c8aab2c51df
NicolasIannello <nicolas.iannello99@gmail.com>
2023-10-18 17:36:38 -0300

    stats

--- src/main/java/com/eximeisty/creaturesofruneterra/item/ModItemTier.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/item/ModItemTier.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.ForgeTier;
 
 public class ModItemTier {
 
-    public static final ForgeTier SAI = new ForgeTier(2, 1400, 1.5f, 2f, 22,
+    public static final ForgeTier SAI = new ForgeTier(6, 2700, 15.0F, 7.0F, 20,
             null, () -> Ingredient.of(ModItems.REKSAI_CLAW.get()));
     public static final ForgeTier ATLAS = new ForgeTier(4, 1600, 10.0F, 5.0F, 10,
             null, () -> Ingredient.of(ModItems.GEMSTONE.get()));
