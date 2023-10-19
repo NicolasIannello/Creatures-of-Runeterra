f384011bccca6c2bb8e92348dda56813a85217b4
NicolasIannello <nicolas.iannello99@gmail.com>
2023-10-18 21:44:58 -0300

    weights

--- src/main/java/com/eximeisty/creaturesofruneterra/world/gen/ModEntityGeneration.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/world/gen/ModEntityGeneration.java
@@ -14,8 +14,8 @@ import java.util.List;
 @SuppressWarnings("unchecked")
 public class ModEntityGeneration {
     public static void onEntitySpawn(final BiomeLoadingEvent event) {
-        addEntityToSpecificBiomes(event, ModEntityTypes.XERSAI_HATCHLING.get(), 35, 2, 4, Biomes.DESERT,Biomes.BADLANDS,Biomes.WOODED_BADLANDS,Biomes.ERODED_BADLANDS, Biomes.DRIPSTONE_CAVES);
-        addEntityToSpecificBiomes(event, ModEntityTypes.PORO.get(), 45, 1, 3, Biomes.SNOWY_BEACH, Biomes.SNOWY_PLAINS, Biomes.SNOWY_TAIGA, Biomes.SNOWY_SLOPES, Biomes.ICE_SPIKES, Biomes.FROZEN_PEAKS, Biomes.FROZEN_RIVER, Biomes.GROVE);
+        addEntityToSpecificBiomes(event, ModEntityTypes.XERSAI_HATCHLING.get(), 25, 2, 4, Biomes.DESERT,Biomes.BADLANDS,Biomes.WOODED_BADLANDS,Biomes.ERODED_BADLANDS, Biomes.DRIPSTONE_CAVES);
+        addEntityToSpecificBiomes(event, ModEntityTypes.PORO.get(), 30, 1, 3, Biomes.SNOWY_BEACH, Biomes.SNOWY_PLAINS, Biomes.SNOWY_TAIGA, Biomes.SNOWY_SLOPES, Biomes.ICE_SPIKES, Biomes.FROZEN_PEAKS, Biomes.FROZEN_RIVER, Biomes.GROVE);
     }
 
     private static void addEntityToSpecificBiomes(BiomeLoadingEvent event, EntityType<?> type, int weight, int minCount, int maxCount, ResourceKey<Biome>... biomes) {
