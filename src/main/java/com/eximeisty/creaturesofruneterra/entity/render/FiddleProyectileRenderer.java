df2c688fe288c102c66588b7fb8e1e47ce93ae7b
NicolasIannello <nicolas.iannello99@gmail.com>
2023-11-02 15:59:44 -0300

    textures

--- src/main/java/com/eximeisty/creaturesofruneterra/entity/render/FiddleProyectileRenderer.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/entity/render/FiddleProyectileRenderer.java
@@ -1,8 +1,8 @@
 package com.eximeisty.creaturesofruneterra.entity.render;
 
-import com.eximeisty.creaturesofruneterra.CreaturesofRuneterra;
 import com.eximeisty.creaturesofruneterra.entity.custom.FiddleProyectileEntity;
 
+import com.eximeisty.creaturesofruneterra.entity.custom.FiddlesticksEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Matrix3f;
@@ -15,13 +15,19 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class FiddleProyectileRenderer extends EntityRenderer<FiddleProyectileEntity> {
-   private static final ResourceLocation TEXTURE = new ResourceLocation(CreaturesofRuneterra.MOD_ID, "textures/entity/empty.png");
+   private static final ResourceLocation TEXTURE = new ResourceLocation("minecraft", "textures/particle/effect_5.png");
    private static final RenderType RENDER_TYPE = RenderType.entityCutoutNoCull(TEXTURE);
+   private static final ResourceLocation TEXTURE3 = new ResourceLocation("minecraft", "textures/particle/soul_0.png");
+   private static final RenderType RENDER_TYPE3 = RenderType.entityCutoutNoCull(TEXTURE3);
+   private static final ResourceLocation TEXTURE5 = new ResourceLocation("minecraft", "textures/particle/effect_3.png");
+   private static final RenderType RENDER_TYPE5 = RenderType.entityCutoutNoCull(TEXTURE5);
 
    public FiddleProyectileRenderer(EntityRendererProvider.Context manager) {
       super(manager);
@@ -33,14 +39,21 @@ public class FiddleProyectileRenderer extends EntityRenderer<FiddleProyectileEnt
 
    public void render(FiddleProyectileEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight) {
       pMatrixStack.pushPose();
-      pMatrixStack.scale(0.25F, 0.25F, 0.25F);
+      pMatrixStack.scale(1F, 1F, 1F);
       pMatrixStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
       pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
       PoseStack.Pose posestack$pose = pMatrixStack.last();
       Matrix4f matrix4f = posestack$pose.pose();
       Matrix3f matrix3f = posestack$pose.normal();
-      VertexConsumer vertexconsumer = pBuffer.getBuffer(RENDER_TYPE);
-      vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 0, 0, 1);
+      Entity owner = pEntity.getOwner();
+      VertexConsumer vertexconsumer;
+      if(owner instanceof FiddlesticksEntity) {
+         vertexconsumer = pBuffer.getBuffer(RENDER_TYPE3);
+      }else if (owner instanceof Player){
+         vertexconsumer = pBuffer.getBuffer(RENDER_TYPE5);
+      }else{
+         vertexconsumer = pBuffer.getBuffer(RENDER_TYPE);
+      }      vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 0, 0, 1);
       vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 0, 1, 1);
       vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 1, 1, 0);
       vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 1, 0, 0);
