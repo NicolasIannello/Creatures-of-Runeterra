8b919b18838f385f668e4c09c24db636f176e13c
NicolasIannello <nicolas.iannello99@gmail.com>
2023-11-04 16:51:30 -0300

    reach

--- src/main/java/com/eximeisty/creaturesofruneterra/item/custom/FiddleScythe.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/item/custom/FiddleScythe.java
@@ -2,16 +2,21 @@ package com.eximeisty.creaturesofruneterra.item.custom;
 
 import com.eximeisty.creaturesofruneterra.item.client.fiddleScythe.FiddleScytheRenderer;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.common.ForgeMod;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -19,31 +24,32 @@ import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 
 public class FiddleScythe extends SwordItem implements GeoItem {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    public String controllerName = "controller";
-    // protected static final UUID REACH = UUID.fromString("f7e4f5c8-2463-4660-bfd3-0de7ac8f7720");
-    // private AttributeModifier reachModifier, damageModifier, speedModifier;
-    // private Multimap<Attribute, AttributeModifier> attributeModifiers;
+    //public String controllerName = "controller";
+     protected static final UUID REACH = UUID.fromString("f7e4f5c8-2463-4660-bfd3-0de7ac8f7720");
+     private AttributeModifier reachModifier, damageModifier, speedModifier;
+     private Multimap<Attribute, AttributeModifier> attributeModifiers;
 
     public FiddleScythe(Tier tier, int attackDamageIn, float attackSpeedIn, Properties builderIn) {
         super(tier, attackDamageIn, attackSpeedIn, builderIn);
-        // reachModifier = new AttributeModifier(REACH, "reach_distance", 2, AttributeModifier.Operation.ADDITION);
-        // damageModifier = new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (float)attackDamageIn+tier.getAttackDamage(), AttributeModifier.Operation.ADDITION);
-        // speedModifier = new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", attackSpeedIn, AttributeModifier.Operation.ADDITION);
+         reachModifier = new AttributeModifier(REACH, "reach_distance", 1.5, AttributeModifier.Operation.ADDITION);
+         damageModifier = new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", (float)attackDamageIn+tier.getAttackDamageBonus(), AttributeModifier.Operation.ADDITION);
+         speedModifier = new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", attackSpeedIn, AttributeModifier.Operation.ADDITION);
     }
 
-    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+//    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         // ItemStack item = entityIn.getHeldEquipment().iterator().next();
         // if(item==stack && !((PlayerEntity)entityIn).getAttribute(ForgeMod.REACH_DISTANCE.get()).hasModifier(reachModifier)){
         //     ((PlayerEntity)entityIn).getAttribute(ForgeMod.REACH_DISTANCE.get()).applyNonPersistentModifier(reachModifier);
         // }else if(!(item==stack)){
         //     ((PlayerEntity)entityIn).getAttribute(ForgeMod.REACH_DISTANCE.get()).removeModifier(reachModifier);
         // }
-    }
+//    }
 
     // public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerentity, Hand handIn) {
     //     ItemStack stack = playerentity.getHeldItem(handIn);
@@ -77,14 +83,14 @@ public class FiddleScythe extends SwordItem implements GeoItem {
         return this.cache;
     }
     
-    // public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
-    //     Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-    //     builder.put(ForgeMod.REACH_DISTANCE.get(), reachModifier);
-    //     builder.put(Attributes.ATTACK_DAMAGE, damageModifier);
-    //     builder.put(Attributes.ATTACK_SPEED, speedModifier);
-    //     this.attributeModifiers = builder.build();
-    //     return equipmentSlot == EquipmentSlotType.MAINHAND ? attributeModifiers : super.getAttributeModifiers(equipmentSlot);
-    // }
+     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot equipmentSlot, ItemStack stack) {
+         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+         builder.put(ForgeMod.ENTITY_REACH.get(), reachModifier);
+         builder.put(Attributes.ATTACK_DAMAGE, damageModifier);
+         builder.put(Attributes.ATTACK_SPEED, speedModifier);
+         this.attributeModifiers = builder.build();
+         return equipmentSlot == EquipmentSlot.MAINHAND ? attributeModifiers : super.getAttributeModifiers(equipmentSlot, stack);
+     }
 
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
