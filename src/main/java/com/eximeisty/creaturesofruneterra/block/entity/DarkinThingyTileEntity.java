40efc16c207c4aa7eb8bab99ae37bd93b3834cf6
NicolasIannello <nicolas.iannello99@gmail.com>
2023-11-18 14:34:21 -0300

    stuff

--- src/main/java/com/eximeisty/creaturesofruneterra/block/entity/DarkinThingyTileEntity.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/entity/DarkinThingyTileEntity.java
@@ -13,6 +13,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.SwordItem;
@@ -32,7 +33,21 @@ import javax.annotation.Nullable;
 
 public class DarkinThingyTileEntity extends BlockEntity implements GeoBlockEntity {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    public final ItemStackHandler itemHandler = new ItemStackHandler(1);
+    public final ItemStackHandler itemHandler = new ItemStackHandler(1){
+        @Override
+        public int getSlotLimit(int slot) {
+            switch (slot) {
+                case 0: return 1;
+                default: return super.getSlotLimit(slot);
+            }
+        }
+
+        @Override
+        protected void onContentsChanged(int slot){
+            setChanged();
+            level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_ALL);
+        }
+    };
     private boolean eye = false;
     private boolean nw = false;
     public int ticks = 0;
@@ -76,60 +91,67 @@ public class DarkinThingyTileEntity extends BlockEntity implements GeoBlockEntit
 
     public void tick() {
         ItemStack item = itemHandler.getStackInSlot(0);
-        if(item.is(Items.IRON_HOE)){
-            BlockEntity tileentity = getLevel().getBlockEntity(worldPosition.east(3));
-            BlockEntity tileentity2 = getLevel().getBlockEntity(worldPosition.west(3));
-            if(tileentity instanceof DarkinThingyTileEntity && tileentity2 instanceof DarkinThingyTileEntity){
-                if(((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0).is(Items.NETHER_WART)) nw = true;
-                if(((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0).is(Items.NETHER_WART)) nw = true;
-                if(((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0).is(Items.ENDER_EYE)) eye = true;
-                if(((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0).is(Items.ENDER_EYE)) eye = true;
-                if(nw && eye) {
-                    nw = false; eye = false;
-                    ticks++;
-                    if(ticks%20==0) getLevel().playSound(null, worldPosition, SoundEvents.BLAZE_SHOOT, SoundSource.BLOCKS, 1F, 1F);
-                    if(ticks>100){
-                        setAndDestroyPedestals(tileentity, tileentity2, new ItemStack(ModItems.RHAAST.get()));
-                        ticks=0;
-                    }
-                    this.getPersistentData().putInt("ticks", ticks);
-                    this.setChanged();
-                    this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
+        if(getLevel().dimension()==Level.NETHER){
+            if(item.is(Items.IRON_HOE) || item.getItem() instanceof SwordItem){
+                BlockEntity tileentity;
+                BlockEntity tileentity2;
+                if(getLevel().getBlockEntity(worldPosition.east(3))!=null){
+                    tileentity = getLevel().getBlockEntity(worldPosition.east(3));
+                    tileentity2 = getLevel().getBlockEntity(worldPosition.west(3));
+                    this.getPersistentData().putBoolean("ns", false);
+                }else{
+                    tileentity = getLevel().getBlockEntity(worldPosition.north(3));
+                    tileentity2 = getLevel().getBlockEntity(worldPosition.south(3));
+                    this.getPersistentData().putBoolean("ns", true);
                 }
-            }
-        }
-
-        if(item.getItem() instanceof SwordItem){
-            if(((SwordItem)item.getItem()).getTier()== ModItemTier.DARKIN){
-                BlockEntity tileentity = getLevel().getBlockEntity(worldPosition.east(3));
-                BlockEntity tileentity2 = getLevel().getBlockEntity(worldPosition.west(3));
-                if(tileentity instanceof DarkinThingyTileEntity && tileentity2 instanceof DarkinThingyTileEntity){
-                    ItemStack tileItem = ((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0);
-                    ItemStack tileItem2 = ((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0);
-                    if(tileItem.is(tileItem2.getItem()) && tileItem2.getItem() instanceof SwordItem){
-                        ticks++;
-                        if(ticks%20==0) getLevel().playSound(null, worldPosition, SoundEvents.BLAZE_SHOOT, SoundSource.BLOCKS, 1F, 1F);
-                         if(ticks>100){
-                            int damage = (int) ((SwordItem) tileItem.getItem()).getDamage() + 1;
-                            ItemStack darkinweapon = new ItemStack(ModItems.RHAAST.get());
-                            item.getAllEnchantments().forEach(darkinweapon::enchant);
-                            darkinweapon.getAttributeModifiers(EquipmentSlot.MAINHAND).forEach((atr, modifier) -> {
-                                if (atr == Attributes.ATTACK_DAMAGE) {
-                                    darkinweapon.addAttributeModifier(atr, new AttributeModifier(modifier.getId(), modifier.getName(), damage, modifier.getOperation()), EquipmentSlot.MAINHAND);
-                                } else {
-                                    darkinweapon.addAttributeModifier(atr, modifier, EquipmentSlot.MAINHAND);
-                                }
-                            });
-                            setAndDestroyPedestals(tileentity, tileentity2, darkinweapon);
-                            ticks=0;
+                this.setChanged();
+                this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
+                if(tileentity instanceof DarkinThingyTileEntity && tileentity2 instanceof DarkinThingyTileEntity) {
+                    if(item.is(Items.IRON_HOE)){
+                        if(((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0).is(Items.NETHER_WART)) nw = true;
+                        if(((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0).is(Items.NETHER_WART)) nw = true;
+                        if(((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0).is(Items.ENDER_EYE)) eye = true;
+                        if(((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0).is(Items.ENDER_EYE)) eye = true;
+                        if(nw && eye) {
+                            nw = false; eye = false;
+                            ticker(null, null, null);
+                            if(ticks > 100) ticker(tileentity, tileentity2, new ItemStack(ModItems.RHAAST.get()));
                         }
-                        this.getPersistentData().putInt("ticks", ticks);
-                        this.setChanged();
-                        this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
+                    }else if(item.getItem() instanceof SwordItem ? ((SwordItem)item.getItem()).getTier()==ModItemTier.DARKIN : false){
+                        ItemStack tileItem = ((DarkinThingyTileEntity) tileentity).itemHandler.getStackInSlot(0);
+                        ItemStack tileItem2 = ((DarkinThingyTileEntity) tileentity2).itemHandler.getStackInSlot(0);
+                        if(tileItem.is(tileItem2.getItem()) && tileItem2.getItem() instanceof SwordItem) {
+                            ticker(null, null, null);
+                            if(ticks > 100) {
+                                int damage = (int) ((SwordItem) tileItem.getItem()).getDamage() + 1;
+                                ItemStack darkinweapon = new ItemStack(ModItems.RHAAST.get());
+                                item.getAllEnchantments().forEach(darkinweapon::enchant);
+                                darkinweapon.getAttributeModifiers(EquipmentSlot.MAINHAND).forEach((atr, modifier) -> {
+                                    if (atr == Attributes.ATTACK_DAMAGE) {
+                                        darkinweapon.addAttributeModifier(atr, new AttributeModifier(modifier.getId(), modifier.getName(), damage, modifier.getOperation()), EquipmentSlot.MAINHAND);
+                                    } else {
+                                        darkinweapon.addAttributeModifier(atr, modifier, EquipmentSlot.MAINHAND);
+                                    }
+                                });
+                                ticker(tileentity, tileentity2, darkinweapon);
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+
+    public void ticker(@Nullable BlockEntity tile, @Nullable BlockEntity tile2, @Nullable ItemStack item){
+        ticks++;
+        if(ticks % 20 == 0) getLevel().playSound(null, worldPosition, SoundEvents.BLAZE_SHOOT, SoundSource.BLOCKS, 1F, 1F);
+        if(item!=null && tile!=null && tile2!=null){
+            ticks=0;
+            setAndDestroyPedestals(tile, tile2, item);
+        }
+        this.getPersistentData().putInt("ticks", ticks);
+        this.setChanged();
+        this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
     }
 
     public void setAndDestroyPedestals(BlockEntity tile, BlockEntity tile2, ItemStack item){
@@ -149,14 +171,6 @@ public class DarkinThingyTileEntity extends BlockEntity implements GeoBlockEntit
             stack.shrink(i-1);
             itemHandler.setStackInSlot(0, stack);
         }
-        this.setChanged();
-        this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
-    }
-
-    @Override
-    public void setRemoved() {
-        level.addFreshEntity(new ItemEntity(level, worldPosition.getX(), worldPosition.getY()+1, worldPosition.getZ(), itemHandler.getStackInSlot(0)));
-        super.setRemoved();
     }
 
     @Override
