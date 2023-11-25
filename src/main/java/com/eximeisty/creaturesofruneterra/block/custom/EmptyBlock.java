2c87adfc6d613b2e4246a425b8b58b4b917cdb29
NicolasIannello <nicolas.iannello99@gmail.com>
2023-11-24 23:24:11 -0300

    no watter logged

--- src/main/java/com/eximeisty/creaturesofruneterra/block/custom/EmptyBlock.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/block/custom/EmptyBlock.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class EmptyBlock extends BaseCoralPlantTypeBlock {
+public class EmptyBlock extends Block {
     private static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 4.0D, 14.0D);
 
     public EmptyBlock(Properties p_49161_) {
