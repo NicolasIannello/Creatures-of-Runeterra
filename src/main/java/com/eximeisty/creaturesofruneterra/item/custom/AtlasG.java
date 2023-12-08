5fd3b65b3dacf6ba78a187f54ffc949f33ad7896
NicolasIannello <nicolas.iannello99@gmail.com>
2023-12-08 20:38:56 -0300

    description / dash behaviour / dash-poundticks-y

--- src/main/java/com/eximeisty/creaturesofruneterra/item/custom/AtlasG.java
+++ src/main/java/com/eximeisty/creaturesofruneterra/item/custom/AtlasG.java
@@ -1,9 +1,11 @@
 package com.eximeisty.creaturesofruneterra.item.custom;
 
 import com.eximeisty.creaturesofruneterra.item.client.atlasg.AtlasGRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -12,9 +14,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.PickaxeItem;
-import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -32,6 +32,7 @@ import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class AtlasG extends PickaxeItem implements GeoItem {
@@ -40,6 +41,9 @@ public class AtlasG extends PickaxeItem implements GeoItem {
     private int dashTicks=0;
     private int soundTicks=0;
     private boolean pound=false;
+    private boolean dash=false;
+    private int poundTicks=0;
+    private double y;
     private Vec3 motion;
     private static final RawAnimation CHARGE_ANIM = RawAnimation.begin().then("animation.atlasg.charge", Animation.LoopType.PLAY_ONCE).then("animation.atlasg.full", Animation.LoopType.LOOP);
     private static final RawAnimation CHARGE2_ANIM = RawAnimation.begin().then("animation.atlasg.charge2", Animation.LoopType.PLAY_ONCE).then("animation.atlasg.full2", Animation.LoopType.LOOP);
@@ -59,6 +63,7 @@ public class AtlasG extends PickaxeItem implements GeoItem {
 
     public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         Iterator<ItemStack> item = entityIn.getHandSlots().iterator();
+        Iterator<ItemStack> item2 = entityIn.getHandSlots().iterator();
         if(item.next()==stack && hand) hand=false;
         if(item.next()==stack && !hand) hand=true;
         if (!worldIn.isClientSide && !isCharged(stack)) {
@@ -76,18 +81,28 @@ public class AtlasG extends PickaxeItem implements GeoItem {
             }
         }
         if(getState(stack)==3){
-            dashTicks++;
-            attackBB(entityIn.getBoundingBox().expandTowards(0.5, 0, 0.5).expandTowards(-0.5, 0, -0.5), entityIn);
-            breakBB(entityIn.getBoundingBox().expandTowards(0.5, 0, 0.5).expandTowards(-0.5, 0, -0.5).move(entityIn.getLookAngle().x*1.5, 0, entityIn.getLookAngle().z*1.5), entityIn, worldIn);
-            if(entityIn.fallDistance>=7) pound=true;
-            if(dashTicks>=20 && (!entityIn.onGround())) dashTicks-=3;
-            if(entityIn.onGround() && pound){
+            if(dashTicks==0) y = entityIn.getY()+5;
+            if(dash){
+                dashTicks++;
+                attackBB(entityIn.getBoundingBox().expandTowards(0.5, 0, 0.5).expandTowards(-0.5, 0, -0.5), entityIn);
+                breakBB(entityIn.getBoundingBox().expandTowards(1, 0, 1).expandTowards(-1, 0, -1).move(entityIn.getLookAngle().x * 1.5, 0, entityIn.getLookAngle().z * 1.5), entityIn, worldIn);
+            }
+            if(entityIn.fallDistance>=7) pound = dash = true;
+            if(entityIn.onGround() && (pound || poundTicks>0)){
+                poundTicks++;
                 breakBB(entityIn.getBoundingBox().expandTowards(1, -2, 1).expandTowards(-1, 0, -1).contract(0, 2, 0), entityIn, worldIn);
                 breakBB(entityIn.getBoundingBox().expandTowards(2, -1, 2).expandTowards(-2, 0, -2).contract(0, 2, 0), entityIn, worldIn);
                 attackBB(entityIn.getBoundingBox().expandTowards(2, 0, 2).expandTowards(-2, 0, -2), entityIn);
                 pound=false;
+                if(poundTicks>=3) poundTicks=0;
             }
-            if(dashTicks>=20) setState(stack, 1);
+            if(y<entityIn.getY()) {
+                y = entityIn.getY() + 5; pound = dash = false;
+            }
+            if(y < entityIn.getY() || (dashTicks>15 && entityIn.onGround()) || (item2.next()!=stack && item2.next()!=stack)) {
+                dashTicks = poundTicks = 0; dash = pound = false;
+                setState(stack, 1);
+            }
         }
     }
 
@@ -113,6 +128,7 @@ public class AtlasG extends PickaxeItem implements GeoItem {
         if(isCharged(stack)){
             worldIn.playSound(playerentity, playerentity.blockPosition(), SoundEvents.PISTON_EXTEND, SoundSource.PLAYERS, 5, 0.9f);
             setState(stack, 3);
+            dash=true;
             playerentity.getCooldowns().addCooldown(this, 20);
             motion = new Vec3(playerentity.getLookAngle().x*2, 0.1, playerentity.getLookAngle().z*2);
             playerentity.setDeltaMovement(motion);
@@ -184,6 +200,11 @@ public class AtlasG extends PickaxeItem implements GeoItem {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
+    }
+
+    public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        String quote = "["+ Minecraft.getInstance().options.keyUse.getKey().toString().replace("keyboard.", "").replace("."," ").replace("key","")+" ] to use charge, then dash";
+        tooltip.add(Component.nullToEmpty(quote));
     }
 
     @Override
